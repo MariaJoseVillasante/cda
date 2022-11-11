38d5419bc2e18c1c776b09d44261cf88b506d7d5
MariaJoseVillasante <pepavillasante@yahoo.com>
2022-11-11 15:10:25 -0300

    Rich comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,5 @@ class Post < ApplicationRecord
     # length: { minimun: 5, maximum: 50 }
     # length: { minimun: 10, maximum: 1000 }
     belongs_to :user
+    has_many :comments, dependent: :destroy
 end
