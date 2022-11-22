7a8164406a8290caaf6f0cd56786e275ab4dab09
MariaJoseVillasante <pepavillasante@yahoo.com>
2022-11-22 20:12:37 -0300

    Agregando Drag and Drop

---
+++ app/controllers/drag_controller.rb
@@ -0,0 +1,11 @@
+class DragController < ApplicationController
+    def project 
+        @project = Project.find(drag_project_params[:id])
+        @project.insert_at(drag_params[:position].to_i + 1)
+    end 
+
+    private
+    def drag_project_params
+        params.require(:resource).permit(:id, :position)
+    end
+end
